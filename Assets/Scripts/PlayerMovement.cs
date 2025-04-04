5fed237e2d89b9c40e7b227821f7fbd795b1b32e
Leonardo Spilere <leonardo.spilere@gmail.com>
2025-04-03 17:43:32 -0700

    temos un juego

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private static readonly int Roll = Animator.StringToHash("Roll");
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    private Vector2 movement;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        
+        CheckMovementInput();
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Roll");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    
+    void CheckMovementInput()
+    {
+        if (movement.x != 0)
+        {
+            animator.SetBool("Run", true);
+            spriteRenderer.flipX = movement.x < 0;
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+        }
+    }
+}
