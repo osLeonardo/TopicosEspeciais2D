31cde913fd071e558f2a586d3d9b7bba1e4a80d7
Leonardo Spilere <leonardo.spilere@gmail.com>
2025-04-05 17:45:45 -0300

    criado prefab de inimigo e reorganizado estrutura de scripts

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemyWalker : MonoBehaviour
+{
+    public float speed = 2f;
+    public bool movingRight = false;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 direction = movingRight ? Vector2.right : Vector2.left;
+        rb.linearVelocity = new Vector2(direction.x * speed, rb.linearVelocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        movingRight = !movingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
